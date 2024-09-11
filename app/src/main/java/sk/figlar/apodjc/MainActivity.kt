290ad7631bb8be2b56e2d3fe4fe419fd44f1cb21
figlo <miro@figlar.sk>
2024-09-11 08:29:38 +0200

    Improve displaying of items in gallery

--- app/src/main/java/sk/figlar/apodjc/MainActivity.kt
+++ app/src/main/java/sk/figlar/apodjc/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -17,14 +18,19 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import kotlinx.coroutines.launch
 import sk.figlar.apodjc.api.ApodApiModel
 import sk.figlar.apodjc.ui.theme.APODJCTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,19 +51,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ApodGallery(modifier: Modifier = Modifier) {
-    val scope = rememberCoroutineScope()
     var apods by remember { mutableStateOf<List<ApodApiModel>>(emptyList()) }
 
+    // calculating image size (width and height)
+    val configuration = LocalConfiguration.current
+    val imageSize = (configuration.screenWidthDp / 3).dp
+
     LaunchedEffect(true) {
-        scope.launch {
-            apods = ApodRepository().getApodApiModels()
-        }
+        apods = ApodRepository().getApodApiModels()
     }
 
+    val apodsWithImages = apods.filter { it.mediaType == "image" }
     LazyVerticalGrid(columns = GridCells.Fixed(3), modifier = modifier) {
-        items(apods) { apod ->
+        items(apodsWithImages.reversed()) { apod ->
             Column {
-                AsyncImage(model = apod.url, contentDescription = "")
+                AsyncImage(
+                    model = apod.url,
+                    placeholder = painterResource(R.drawable.placeholder),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(imageSize)
+                )
+
+                val localDate = LocalDate.parse(apod.date)
+                val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+                val formattedLocalDate = localDate.format(formatter)
+
+                Text(text = formattedLocalDate)
                 Text(text = apod.title)
             }
         }
